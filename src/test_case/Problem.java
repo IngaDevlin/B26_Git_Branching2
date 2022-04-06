5cd3b84264293c8594be6b60a067131cb5a8d666
IngaDevlin <98770504+IngaDevlin@users.noreply.github.com>
2022-04-06 19:45:48 -0400

    created main and loop and comments

---
+++ src/test_case/Problem.java
@@ -0,0 +1,12 @@
+package test_case;
+
+public class Problem {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 6 ; i++) {
+            System.out.println(j);
+
+        }
+        // useful info
+    }
+}
