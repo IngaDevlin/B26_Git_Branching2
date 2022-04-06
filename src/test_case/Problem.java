23befcad36c17e24e362a00772af7e5264a0014a
IngaDevlin <98770504+IngaDevlin@users.noreply.github.com>
2022-04-06 19:46:08 -0400

    created main and loop and comments

--- src/test_case/Problem.java
+++ src/test_case/Problem.java
@@ -3,7 +3,7 @@ package test_case;
 public class Problem {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 6 ; i++) {
+        for (int j = 0; j < 6 ; j++) {
             System.out.println(j);
 
         }
